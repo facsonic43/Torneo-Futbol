b9adb1090f092ec5d160a4f6b592b55193a74c54
JoacoToniut6 <joacotoniut@gmail.com>
2026-08-31 00:56:18 +0000

    Update City and Stadium classes with validations

--- model/match/Stadium.java
+++ model/match/Stadium.java
@@ -1,4 +1,33 @@
 package model.match;
 
 public class Stadium {
+
+    private int id;
+    private String name;
+    private int cityId;
+
+    public Stadium(String name, int cityId) {
+        this.name = name;
+        this.cityId = cityId;
+    }
+
+    public Stadium(int id, String name, int cityId) {
+        this.id = id;
+        this.name = name;
+        this.cityId = cityId;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getCityId() { return cityId; }
+    public void setCityId(int cityId) { this.cityId = cityId; }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
