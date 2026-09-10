f5f33be349e62052486c1f013b6c752c77100b50
JT <joacotoniut@gmail.com>
2026-09-10 12:41:41 -0300

    Test bd

---
+++ src/main/java/db/DatabaseConnection.java
@@ -0,0 +1,23 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/copa_clubes";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1234";
+
+    private static Connection connection;
+
+    private DatabaseConnection() { }
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+}
